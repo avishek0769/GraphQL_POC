a90a199dcf25326406068a0dea1abd8e405cc8f6
avishek0769 <avishekadhikary42@gmail.com>
2026-08-03 21:09:06 +0530

    Define the Query and Mutation

--- src/graphql/user/mutations.ts
+++ src/graphql/user/mutations.ts
@@ -1,3 +1,8 @@
 export const mutations = `#graphql
-    
+    createUser(name: String!, email: String!, password: String!): String!
+    loginUser(email: String!, password: String!): User!
+    changePassword(password: String!): Boolean!
+
+    createPost(text: String!): String!
+    editPost(text: String!): String!
 `
--- src/graphql/user/queries.ts
+++ src/graphql/user/queries.ts
@@ -1,3 +1,8 @@
 export const queries = `#graphql
-    
+    getUserToken(id: ID!): String!
+    getCurrentUser(): User!
+    getUsers(): [User!]
+
+    getThreadsByUser(id: ID!): [Thread!]
+    getCurrentUserThreads(): [Thread!]
 `
