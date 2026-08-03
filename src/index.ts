021ff6967d7f00b6ef54a4bacab61dbbb964545d
avishek0769 <avishekadhikary42@gmail.com>
2026-08-03 16:12:00 +0530

    Create nested Todo schema and write it's query resolvers

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,33 @@ async function startGraphqlServer() {
                 isAdmin: Boolean!
             }
 
+            type Todo {
+                id: ID!
+                text: String!
+                userId: ID!
+                user: User
+            }
+
             type Query {
                 getName: String!
                 getUsers: [User]
                 getUser: User!
+
+                getTodos: [Todo!]
+                getTodo(id: ID!): Todo!
             }
         `,
         resolvers: {
+            Todo: {
+                user: (todo) => ({ name: "Avishek", phone: todo.userId, isAdmin: true }) // You can find this on DB with the -> todo.userId
+            },
             Query: {
                 getName: () => "Avishek Okay!!",
                 getUsers: () => [{ name: "Avishek", phone: 989898, isAdmin: true }],
                 getUser: () => ({ name: "Avishek", phone: 989898, isAdmin: true }),
+
+                getTodos: () => [{ id: 1, text: "This is a todo", userId: 1 }],
+                getTodo: (parent, { id }) => ({ id, text: "This is a todo", userId: 1 }) // Find the todo in DB with "id"
             },
             // Mutation: {
 
